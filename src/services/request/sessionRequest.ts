b48aa8bf7085085eccd2af85ce65b11e47f74257
unknown <joshua@tebengan.com>
2025-01-21 11:44:14 +0700

    http call

--- src/services/request/sessionRequest.ts
+++ src/services/request/sessionRequest.ts
@@ -6,13 +6,23 @@ export const fetchSessionSetting = createAsyncThunk<SessionProps, number>(
   "fetchSessionSetting",
   async (deviceId, { rejectWithValue }) => {
     try {
-      const res = await Api.get({
-        url: "/heartbeat",
-        params: { deviceId },
-        showLog: true,
+      // const res = await Api.get({
+      //   url: "/heartbeat",
+      //   params: { deviceId },
+      //   showLog: true,
+      // });
+
+      const response = await fetch(`http://35.240.182.255:9556/heartbeat?deviceId=${deviceId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
 
-      return res;
+      const data = await response.json();
+      console.log("cek data", data);
+
+      return data
     } catch (e) {
       console.log("cek e", e);
 
