5597003bbc3d38882e98452e06255c3e188fe6a3
Osi <osi@tebengan.id>
2025-05-30 08:20:36 +0700

    change value input nominal n hour on session settings

--- src/common/constanta/arrays.ts
+++ src/common/constanta/arrays.ts
@@ -1,4 +1,4 @@
 export const AVAILABLE_PLACE: string[] = ["Terdekat", "Termurah", "Tersedia"];
-export const HOUR_SESSION: string[] = ["30", "60", "90", "720"];
-export const NOMINAL_SESSION: string[] = ["1000", "2000", "5000", "120000"];
+export const HOUR_SESSION: string[] = ["30", "60", "90", "120"];
+export const NOMINAL_SESSION: string[] = ["1000", "2000", "5000", "20000"];
 export const NOMINAL_TOP_UP: string[] = ["5000", "10000", "20000", "50000"];
