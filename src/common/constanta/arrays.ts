60676a62e2ddd68d776783a7a7fe74f82ffe6876
Osi <osi@tebengan.id>
2025-06-05 10:09:31 +0700

    change value default on nominal and hour on session settings

--- src/common/constanta/arrays.ts
+++ src/common/constanta/arrays.ts
@@ -1,4 +1,4 @@
 export const AVAILABLE_PLACE: string[] = ["Terdekat", "Termurah", "Tersedia"];
-export const HOUR_SESSION: string[] = ["30", "60", "90", "120"];
-export const NOMINAL_SESSION: string[] = ["5000", "10000", "15000", "20000"];
+export const HOUR_SESSION: string[] = ["30", "60", "120", "300"];
+export const NOMINAL_SESSION: string[] = ["2000", "5000", "10000", "20000"];
 export const NOMINAL_TOP_UP: string[] = ["5000", "10000", "20000", "50000"];
